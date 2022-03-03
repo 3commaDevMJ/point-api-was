cb238ff3711ee4864a291a321b1f7de20a4eada2
3comma <3comma.devmj@gmail.com>
2022-03-04 00:58:42 +0900

    Initial commit

---
+++ src/main/java/musinsa/api/point/controller/PointController.java
@@ -0,0 +1,16 @@
+package musinsa.api.point.controller;
+
+import lombok.RequiredArgsConstructor;
+import musinsa.api.point.response.PointResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PointController {
+
+
+}
---
+++ src/main/java/musinsa/api/point/response/PointResponse.java
@@ -0,0 +1,13 @@
+package musinsa.api.point.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+public class PointResponse {
+}
